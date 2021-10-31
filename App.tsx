c2f4ac1d1525d6cafda5a76114cea74f258e335b
JulianaV <julianabalta16@gmail.com>
2021-10-30 20:01:33 -0400

    Feat: ButtonAdd created and other changes

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useFonts } from 'expo-font';
 import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
-import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
+import { Rajdhani_500Medium, Rajdhani_600SemiBold, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
 import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'react-native';
 
@@ -13,6 +13,7 @@ export default function app() {
     Inter_400Regular,
     Inter_500Medium,
     Rajdhani_500Medium,
+    Rajdhani_600SemiBold,
     Rajdhani_700Bold,
   });
 
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View } from 'react-native';
-//
+
+//Components
+import { CategorySelect } from '../../components/CategorySelect';
+import { ButtonAdd } from '../../components/ButtonAdd';
 import { Profile } from '../../components/Profile';
+
 //Estilo
 import { styles } from './styles';
 
 export function Home() {
+  const [category, setCategory] = useState('');
+
+  function handleCategorySelect(categoryId: string) {
+    categoryId === category ? setCategory('') : setCategory(categoryId);
+  }
   return (
     <View>
       <View style={styles.header}>
         <Profile />
+        <ButtonAdd />
+      </View>
+
+      <View>
+        <CategorySelect categorySelected={category} setCategory={handleCategorySelect} />
       </View>
     </View>
   );
